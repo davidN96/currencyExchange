04983b74a7e4a0d645eb9f2c4cee9e6cd4648dd7
Dawid Nienajadło <dawidnienajadlo96@gmail.com>
2020-02-20 00:37:02 +0100

    Updates

---
+++ precache-manifest.23a22712c72e3e933e0c0518a815d3de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ace5fbb54f8073fef675a03b494b74",
+    "url": "/currencyExchange/index.html"
+  },
+  {
+    "revision": "dec3ebfa18e7c5fb181c",
+    "url": "/currencyExchange/static/css/main.b0630a03.chunk.css"
+  },
+  {
+    "revision": "522831b5a3de7bb41d38",
+    "url": "/currencyExchange/static/js/2.f0eb3503.chunk.js"
+  },
+  {
+    "revision": "a89090340dd46798274e8d36404839fc",
+    "url": "/currencyExchange/static/js/2.f0eb3503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec3ebfa18e7c5fb181c",
+    "url": "/currencyExchange/static/js/main.0a72e4c8.chunk.js"
+  },
+  {
+    "revision": "de90ea62998667f6f982",
+    "url": "/currencyExchange/static/js/runtime-main.c9320470.js"
+  }
+]);
